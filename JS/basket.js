751d8db163e1d17e3589516b7b9cf71a7a87da96
GunelYusuf <91114321+GunelYusuf@users.noreply.github.com>
2022-01-16 21:15:19 +0400

    fixed basket js

---
+++ JS/basket.js
@@ -0,0 +1,53 @@
+let addBasket = document.querySelectorAll(".addBasket");
+
+if (localStorage.getItem("basket") == null) {
+    localStorage.setItem("basket", JSON.stringify([]));
+}
+
+for (let itemBasket of addBasket) {
+    itemBasket.onclick = function (e) {
+        e.preventDefault();
+
+        let productPrice = Number(this.parentElement.nextElementSibling.querySelector(".totalPrice").innerText);
+        let totalPrice = Number(document.getElementById("totalPrice").innerText);
+
+        totalPrice += productPrice;
+        console.log(totalPrice)
+        document.getElementById("totalPrice").innerText = totalPrice;
+
+        let basket = JSON.parse(localStorage.getItem("basket"));
+        if (localStorage.getItem("basket") == null) {
+            localStorage.setItem("basket", JSON.stringify([]));
+        }
+
+        let name = this.parentElement.previousElementSibling.innerText;
+        let src = this.parentElement.parentElement.previousElementSibling.firstElementChild.getAttribute("src");
+        let data_id = this.parentElement.parentElement.parentElement.getAttribute("data-id");
+        let price = this.parentElement.nextElementSibling.querySelector(".totalPrice").innerText;
+        let existProduct = basket.find(product => product.Id == data_id);
+
+        if (existProduct === undefined) {
+            basket.push({
+                Id: data_id,
+                Name: name,
+                Src: src,
+                Price: price,
+                Count: 1
+            })
+        }
+        else{
+            existProduct.Count+=1;
+        }
+
+        localStorage.setItem("basket", JSON.stringify(basket));
+        CountProductBasket();
+    }
+}
+
+function CountProductBasket(){
+    let basket = JSON.parse(localStorage.getItem("basket"));
+    let productCount = document.getElementById("productCount");
+    productCount.innerText = basket.reduce((total,i)=>total+i.Count,0);
+}
+
+CountProductBasket();
